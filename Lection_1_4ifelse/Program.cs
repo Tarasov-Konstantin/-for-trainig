cc30c5304ad92b37470fb6aa5d5d179be8f8aedd
Konstantin Tarasov <grch@yandex.ru>
2022-09-13 20:00:58 +0300

    hi wee

---
+++ Lection_1_4ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter name");
+string name = Console.ReadLine();
+
+if(name.ToLower() == "wee")
+{
+Console.WriteLine("Horay, it's Wee");
+}
+else
+{
+    Console.WriteLine("Booo!");
+    Console.Write(name);
+}
