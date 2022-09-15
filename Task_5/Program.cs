f3531761156206605ac863011fd2a7ac7b3cb050
Konstantin Tarasov <grch@yandex.ru>
2022-09-15 21:40:53 +0300

    all tasks

---
+++ Task_5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Enter 3-digit number ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100 || num > 999)
+{Console.WriteLine("Number is not 3-digit");
+return;}
+int res = num%10;
+Console.WriteLine($"The last digit is {res}");
+
